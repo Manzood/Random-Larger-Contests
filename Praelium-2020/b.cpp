3f7233621c7078fefe8868dbf314ae92213c229f
Manzood <manzood.naqvi@gmail.com>
2020-10-21 15:10:27 +0530

    Solution for B now works

--- Praelium-2020/b.cpp
+++ Praelium-2020/b.cpp
@@ -4,6 +4,9 @@ using namespace std;
 const int mod = 1000000007;
 #define debug(x) cout << #x << " = " << x << endl;
 
+// I am currently unable to figure out for the life of me why the commented part does not work, but the uncommented code works.
+// it is a complete mystery to me at the moment, because they should be completely identical. I haven't messed with the modulo operation one bit
+
 int32_t main() {
     int t = 1;
     cin >> t;
@@ -13,13 +16,18 @@ int32_t main() {
     for (int i = 3; i <= N; i++) {
         ans[i] = 1;
         if (i % 2 == 0) {
-            ans[i] = ((ans[i]) * (ans[i-1]));
-            ans[i] = ((ans[i]) * (i));
+            // ans[i] = ((ans[i]) * (ans[i-1]));
+            // ans[i] = ((ans[i]) * (i));
+            ans[i] *= i/2 * i/2;
+            ans[i] *= ans[i-2];
         }
         else {
-            ans[i] = ((ans[i]) * ((ans[i-1]/2)));
-            ans[i] = ((ans[i]) * ((i/2 + 1)));
+            // ans[i] = ((ans[i]) * ((ans[i-1]/2)));
+            // ans[i] = ((ans[i]) * ((i/2 + 1)));
+            ans[i] *= i/2 * (i/2+1);
+            ans[i] *= ans[i-2];
         }
+        // if (i < 18) debug(ans[i]);
         ans[i] %= mod;
     }
     while (t--) {
