eb08c7a1a2915af15721b741291694e1635c7d42
Manzood <manzood.naqvi@gmail.com>
2020-10-21 14:35:25 +0530

    Added a few changes while debugging

--- Praelium-2020/b.cpp
+++ Praelium-2020/b.cpp
@@ -2,32 +2,7 @@
 using namespace std;
 #define int long long
 const int mod = 1000000007;
-
-vector<string> vec_splitter(string s) {
-    s += ','; vector<string> res;
-    while(!s.empty()) {
-        res.push_back(s.substr(0, s.find(',')));
-        s = s.substr(s.find(',') + 1);
-    }
-    return res;
-}
-
-void debug_out( vector<string> __attribute__ ((unused)) args, __attribute__ ((unused)) int idx, __attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
-template <typename Head, typename... Tail>
-
-void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
-    if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
-    stringstream ss; ss << H;
-    cerr << args[idx] << " = " << ss.str();
-    debug_out(args, idx + 1, LINE_NUM, T...);
-}
-
-#ifdef local 
-    #define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
-#else 
-    #define debug(...) 42
-#endif
-
+#define debug(x) cout << #x << " = " << x << endl;
 
 int32_t main() {
     int t = 1;
@@ -38,17 +13,14 @@ int32_t main() {
     for (int i = 3; i <= N; i++) {
         ans[i] = 1;
         if (i % 2 == 0) {
-            ans[i] *= ans[i-1];
-            ans[i] *= i;
+            ans[i] = ((ans[i]) * (ans[i-1]));
+            ans[i] = ((ans[i]) * (i));
         }
         else {
-            ans[i] *= ans[i-1]/2;
-            ans[i] *= i/2 + 1;
+            ans[i] = ((ans[i]) * ((ans[i-1]/2)));
+            ans[i] = ((ans[i]) * ((i/2 + 1)));
         }
         ans[i] %= mod;
-        if (i < 18) {
-            debug(i, ans[i]);
-        }
     }
     while (t--) {
         int n;
